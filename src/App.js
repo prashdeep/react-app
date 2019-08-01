785b6e713556ac3862e941edaf8061f92aa24357
pradeep.kumar44@gmail.com <pradeep.kumar44@gmail.com>
2019-08-01 15:34:28 +0530

    committing the code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Sidear from './components/Sidebar';
-import Footer from './components/Footer';
-import Main from './components/Main';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Sidear/>
-      <Main/>
-      <Footer/>
+      <h1> App Component is Loaded !!</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Main from './components/Main';
+import About from './components/About';
+
+const router = (
+    <Router>
+        <ul>
+            <li><Route exact path="/" component={App}/> </li>
+            <li><Route path="/main" component={Main}/></li>
+            <li><Route path="/about" component={About}/></li>
+        </ul>
+    </Router>
+)
+
 
 ReactDOM.render(
-    <App/>, 
+    router, 
     document.getElementById('root'));
 
