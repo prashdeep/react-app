2232b851f550604854ff856b0ba61a1527493db8
pradeep.kumar44@gmail.com <pradeep.kumar44@gmail.com>
2019-08-01 11:55:14 +0530

    committing the changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,21 @@
 import React,{Component} from 'react';
 
 export default class Card extends Component {
-
-    details(){
-        console.log('Came inside the click event handler');
+    constructor(){
+        super();
+        //this.details = this.details.bind(this);
     }
+   /* details(){
+        console.log('Printing the value of this .. inside the details');
+        console.log(this);
+    }*/
     render(){
         return  (
             <div className="card" style={{width: 18 +'rem'}}>
                 <div className="card-body">
                     <h5 className="card-title">{ this.props.title.toUpperCase()}</h5>
                     <p className="card-text">{this.props.body}</p>
-                    <a className="btn btn-primary" onClick={this.details()}>Details</a>
+                    <a className="btn btn-primary" onClick={this.props.selected}>Details</a>
                 </div>
                 </div>
         );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import Card from './Card';
 
-function Main() {
-    const phones=[
-        {
-            name:'IPhone',
-            desc:'Latest Iphone model from Apple'
-        },
-        {
-            name:'One Plus 6',
-            desc:'Flagship phone from OnePlus'
-        },
-        {
-            name:'Samsung S10',
-            desc:'Hight performance..'
-        }
-    ]
-    return (
-        <div className="left">
+export default class Main extends React.Component {
+
+    constructor(){
+        super();
+        console.log(`called from the constructor of Main component..`)
+        this.selectedProduct = this.selectedProduct.bind(this);
+    }
+
+    selectedProduct( data){
+        console.log(this)
+       console.log(`selected product is  ${data}`)
+    }
+
+    componentDidMount(){
+        console.log(`Component is mounted on the DOM`)
+    }
+
+    render(){
+       const phones=[
             {
-              phones.map((phone,index) => <Card key={index} title={phone.name} body={phone.desc} />)
+                name:'IPhone',
+                desc:'Latest Iphone model from Apple'
+            },
+            {
+                name:'One Plus 6',
+                desc:'Flagship phone from OnePlus'
+            },
+            {
+                name:'Samsung S10',
+                desc:'Hight performance..'
             }
-           </div>
-    );
+        ]
+        return (
+            <div className="left">
+                {
+                    phones.map((phone,index) => <Card 
+                                                key={index} 
+                                                title={phone.name} 
+                                                body={phone.desc}
+                                                selected = {this.selectedProduct}/>)
+                }
+            </div>
+        );
+    }
 }
 
-export default Main;
